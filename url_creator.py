904fb6ca192fa476b3a6fd5946fc7cdc54e93b68
Pentek Zsolt <zsoltpentek08@gmail.com>
2020-03-27 14:41:42 +0200

    no message

--- url_creator.py
+++ url_creator.py
@@ -1,6 +1,6 @@
 from os import path
 
-AMAZON_BASE_URL = 'https://www.amazon.com/dp'
+AMAZON_BASE_URL = 'https://www.amazon.com'
 
-def creat_product_url(product_id):
-    return path.join(AMAZON_BASE_URL, product_id)
+def create_product_url(product_id):
+    return path.join(AMAZON_BASE_URL, 'dp', product_id)
