3960191c99fd8c7ca8d9cd9128db93db976a24d9
Pentek Zsolt <zsoltpentek08@gmail.com>
2020-04-05 16:12:12 +0300

    Added basic manager.py

--- manager.py
+++ manager.py
@@ -1,19 +1,62 @@
 from url_creator import create_product_url
-from request import request
 from bs4 import BeautifulSoup
+from utils.request import request
+from utils import utils
+from parser import Parser
+import json, re
+import validators
 
-product_url = create_product_url('B07THHQMHM')
-r = request(product_url)
-soup = BeautifulSoup(r.content, 'lxml')
-# results = soup.find_all('span')
-# print(results)
-title = soup.find('span', attrs={'id':'productTitle'}).encode_contents().decode('utf-8').strip()
-print('title:', title)
-with open('testing.txt', 'w') as f:
-    f.write(r.content.decode('utf-8'))
-    
-# print(soup.prettify())
+# product_url = create_product_url(product_id)
+
+products_page_input = input("Enter url of the products page: ")
+check_url = validators.url(products_page_input)
+if check_url==True:
+    url_products = request(products_page_input)
+else:
+    print("Invalid url")
+
+my_parser = Parser()
+product_ids = my_parser.parse_page(url_products) #get the list of the product IDs 
+
+for product_id in product_ids:
+    url_product = create_product_url(product_id)
+    response_details = request(url_product)
+    response_reviews = request('https://www.amazon.com/gp/customer-reviews/aj/private/reviewsGallery/get-data-for-reviews-image-gallery-for-asin?asin='+product_id)
+    product_details = my_parser.parse_product(response_details)
+    product_reviews = my_parser.parse_reviews(response_reviews)
+    full_details = {
+        'product_details': product_details, 
+        'product_reviews': product_reviews
+    }
+    file_name = product_id + '.txt'
+    with open(file_name, 'w') as f:
+        json.dump(full_details, f, indent=4)
 
 
-# .decode('utf-8')
-# print(results)
+
+# print(my_parser.parse_product(r))
+# print(my_parser.parse_reviews(r))
+# with open('testing.txt', 'w') as f:
+#     f.write(r.text)
+
+
+
+
+
+
+
+
+
+
+
+# https://www.amazon.com/s?k=laptop&ref=nb_sb_noss_2
+# https://www.amazon.com/s?k=iphone&ref=nb_sb_noss_2
+# https://www.amazon.com/s?k=toys&ref=nb_sb_noss_2
+
+# soup = BeautifulSoup(r.content, 'lxml')
+
+# images_urls = []
+# json_data = re.search("var obj = jQuery.parseJSON\('(.*)'\);", r.text).group(1)
+# parsed_json = json.loads(json_data)
+
+# print(Parser().parse_product(r))
